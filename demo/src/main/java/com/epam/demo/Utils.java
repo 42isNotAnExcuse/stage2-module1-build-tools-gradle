73fa4af7fb014844342cb1d9b6fdc632800f5e72
Igor Bugaiov <bugayov.igor@nixsolutions.com>
2025-05-07 16:51:40 +0200

    init setup

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -4,6 +4,15 @@ import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        try {
+            return args != null && !args.isEmpty() && args.stream()
+                    .allMatch(n ->
+                            n != null && !n.isEmpty() && !n.equals(" ")
+                                    && (Integer.parseInt(n) > 0
+                                    || Double.parseDouble(n) > 0
+                                    || Long.parseLong(n) > 0));
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,13 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        try {
+            return str != null && !str.isEmpty() && !str.equals(" ")
+                    && (Integer.parseInt(str) > 0
+                    || Double.parseDouble(str) > 0
+                    || Long.parseLong(str) > 0);
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
